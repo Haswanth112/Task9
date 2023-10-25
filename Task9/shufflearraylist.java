916e7c6cb96af766d3cb1b23b83ebb0b3e0d18d8
Haswanth112 <147820442+Haswanth112@users.noreply.github.com>
2023-10-25 18:05:27 +0530

    Update shufflearraylist.java

--- Task9/shufflearraylist.java
+++ Task9/shufflearraylist.java
@@ -1,21 +1,16 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
-public class shufflearraylist 
-{
-    public static void main(String[] args) 
-    {
-     ArrayList<String> arrayList = new ArrayList<>();
-     arrayList.add("Haswanth");
-     arrayList.add("virat kohli");
-     arrayList.add("scahin Tendulakr");
-     arrayList.add("Rohit Sharma");
-     arrayList.add("Kl Rahul");
-     Collections.shuffle(arrayList);
-     System.out.println("Shuffled ArrayList:");
-        for (String element : arrayList) 
-        {
-            System.out.println(element);
+public class ElementExistsInArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Rohith");
+        list.add("Virat");
+        list.add("Dhoni");
+        list.add("yuvaraj");
+        String elementToFind = "Rohith";
+        if (list.contains(elementToFind)) {
+            System.out.println(elementToFind + " exists in the ArrayList.");
+        } else {
+            System.out.println(elementToFind + " does not exist in the ArrayList.");
         }
     }
-}
